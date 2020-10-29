d78ff153d53673d1e6fcee9ebd9e7a956d88334f
Alejandro Triana <alejandro183@gmail.com>
2020-10-29 08:00:41 -0500

    Update client.py

--- service/client.py
+++ service/client.py
@@ -8,7 +8,7 @@ class Client():
         self.preexistence = []
 
     def add_preexistence(self, n_preexistence):
-        """Adds a preexistence to a client""" 
+        """Adds a preexistence to a client"""
         self.preexistence.append(n_preexistence)
         return len(self.preexistence) - 1
 
@@ -16,13 +16,14 @@ class Client():
         """Get a preexistence given the index"""
         if p_index >= len(self.preexistence):
             return 'There is no such preexistence'
-        
-        return self.preexistence[pIndex]
+        return self.preexistence[p_index]
 
     def get_all_preexistence(self):
+        """Get all the preexistence"""
         return self.preexistence
 
     def remove_preexistence(self, n_preexistence):
+        """Removes a preexistence given the index"""
         self.preexistence.pop(n_preexistence)
         return len(self.preexistence) - 1
 
